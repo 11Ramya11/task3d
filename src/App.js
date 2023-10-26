f2ac4b5a23651d9d4a34264c57caacde235e215a
11Ramya11 <105539397+11Ramya11@users.noreply.github.com>
2023-10-26 19:38:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Task3d from './components/Task3d';
+
+function App() {
+  return (
+    <div className="App">
+      <Task3d/>
+    </div>
+  );
+}
+
+export default App;
